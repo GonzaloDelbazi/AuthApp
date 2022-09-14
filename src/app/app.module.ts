2bc92c0d91616ac9449053f10893aaabccba9991
Gonzalo Delbazi <45603924+GonzaloDelbazi@users.noreply.github.com>
2022-09-14 17:56:04 -0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { AuthModule } from '@auth0/auth0-angular';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    PreciosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-226hjj62.us.auth0.com',
+      clientId: 'SAxa0T3dDzzM4Yo7vAQJli4y2Nyyy6h7'
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
